63b707594d23d7ea5927fb4c4296a2d4a01bd120
Alex Nunez <alexanderjnunez98@gmail.com>
2022-03-23 20:16:58 -0700

    Sorts scores in index.html and adds labels to the scores. Javascript adds alerts when a player or ties reach 5. Buttons won't move score past 5.

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let playerScore = 0;
 let computerScore = 0;
 let tempPlayerSelection;
 const btn = document.querySelectorAll('button');
-btn.forEach(function(button){
+btn.forEach(function (button) {
     button.addEventListener('click', () => {
         tempPlayerSelection = button.getAttribute('id');
     });
@@ -69,28 +69,27 @@ function determineWinner(playerSelection, computerSelection) {
         return "tie";
 }
 
+function updateScore(divId, userScore) {
+    document.getElementById(divId).textContent = userScore;
+}
+
 function playRound() {
     const playerSelection = tempPlayerSelection;
-    console.log("Player chose: " + playerSelection);
     const computerSelection = computerPlay();
-    console.log("Computer chose: " + computerSelection);
 
     let roundWinner = determineWinner(playerSelection, computerSelection);
 
     if (roundWinner === "player") {
 
-        console.log("You Win");
         return "player";
 
     }
     else if (roundWinner === "computer") {
 
-        console.log("You Lose");
         return "computer";
     }
     else if (roundWinner === "tie") {
 
-        console.log("Tie");
         return "tie";
     }
 
@@ -103,31 +102,41 @@ function game() {
     let computerScore = 0;
     let ties = 0;
 
-    
-    for (let games = 0; games < 5; games++) {
-
-        let result = playRound();
-
-        if (result === "player") {
-            playerScore++;
-        }
-        else if (result === "computer") {
-            computerScore++;
-        }
-        else {
-            ties++;
+    for (let i = 0; i < btn.length; i++) {
+        btn[i].onclick = function () {
+            if (playerScore === 5 || 
+                computerScore === 5 ||
+                ties === 5) {
+                if (playerScore === 5) {
+                    alert("You Win!");
+                }
+                else if (computerScore === 5) {
+                    alert("You Lose!");
+                }
+                else if (ties === 5) {
+                    alert("You Tie!");
+                }
+            } else {
+                let result = playRound();
+                if (result === "player") {
+                    playerScore++;
+                    updateScore('playerScore', playerScore);
+                    return;
+                }
+                else if (result === "computer") {
+                    computerScore++;
+                    updateScore('computerScore', computerScore);
+                    return;
+                }
+                else {
+                    ties++;
+                    updateScore('ties', ties);
+                    return;
+                }
+            }
         }
     }
 
-    console.log("Player Score: " + playerScore);
-    console.log("Computer Score: " + computerScore);
-    console.log("Ties: " + ties);
-    if (playerScore > computerScore) {
-        return "You Win!";
-    }
-    else if (playerScore < computerScore) {
-        return "You Lose!";
-    }
 }
+game();
 
-container.addEventListener('click', () => playRound())
